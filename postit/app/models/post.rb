5e52189d2d529d500b4fdad10b575b5eebd48dac
Victoria Bagnell <victoria@jbyte.com>
2014-08-26 14:30:15 -0400

    Setup Post-Categories

--- postit/app/models/post.rb
+++ postit/app/models/post.rb
@@ -3,4 +3,6 @@ class Post < ActiveRecord::Base
   # can be
   #  belongs_to :creator, foreign_key: 'user_id', class_name: 'User'
   has_many :comments
+  has_many :post_categories
+  has_many :categories, through: :post_categories
 end
